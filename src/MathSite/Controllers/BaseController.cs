6279b25c169e03f2b404018c83fb82e11181c00e
Andrey Mokeev <andrey@mokeev1995.ru>
2016-10-14 04:01:33 +0300

    added files

---
+++ src/MathSite/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using MathSite.Db;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathSite.Controllers
+{
+	public class BaseController : Controller
+	{
+		protected readonly IMathSiteDbContext DbContext;
+
+		public BaseController(IMathSiteDbContext dbContext)
+		{
+			DbContext = dbContext;
+		}
+	}
+}
---
+++ src/MathSite/Controllers/ErrorsController.cs
@@ -0,0 +1,29 @@
+﻿using MathSite.Db;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathSite.Controllers
+{
+	public class ErrorsController : BaseController
+	{
+		public ErrorsController(IMathSiteDbContext dbContext) : base(dbContext)
+		{
+		}
+
+		[Route("/errors/404")]
+		public IActionResult NotFound404()
+		{
+			Response.StatusCode = 404;
+			return View();
+		}
+
+		[AllowAnonymous]
+		[Route("/errors/403")]
+		public IActionResult Forbidden(string returnUrl)
+		{
+			Response.StatusCode = 403;
+			ViewBag.ReturnUrl = returnUrl;
+			return View();
+		}
+	}
+}
