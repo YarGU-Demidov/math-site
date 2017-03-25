f77bc53b639aa8d040456bb0d27936e0b3fece25
Andrey Mokeev <andrey@mokeev1995.ru>
2017-03-26 01:43:30 +0300

    новые фишки для менюхи

---
+++ src/MathSite/Core/IResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace MathSite.Core
+{
+	public interface IResponse<T>
+	{
+		[JsonProperty("error")]
+		string Error { get; }
+
+		[JsonProperty("result")]
+		string Result { get; }
+		
+		[JsonProperty("data")]
+		T Data { get; }
+	}
+}
