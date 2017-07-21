c4356b8172c45662e8ee2a0d187e9c604702014a
Andrey Mokeev <andrey@mokeev1995.ru>
2017-07-22 02:38:25 +0300

    + added more tests for sha512 pass manager

--- tests/MathSite.Tests.Common/BinaryHashPasswordsManagerTests.cs
+++ tests/MathSite.Tests.Common/BinaryHashPasswordsManagerTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using MathSite.Common.Crypto;
 using Xunit;
 
@@ -6,7 +8,7 @@ namespace MathSite.Tests.Common
 	public class BinaryHashPasswordsManagerTests
 	{
 		[Fact]
-		public void PasswordHash()
+		public void PasswordBytesAreEqual()
 		{
 			const string login = "login";
 			const string pass = "pass";
@@ -18,6 +20,52 @@ namespace MathSite.Tests.Common
 			var result = hasher.PasswordsAreEqual(login, pass, resultBytes);
 
 			Assert.True(result);
+		}
+
+		[Fact]
+		public void LoginNull()
+		{
+			var hasher = new DoubleSha512HashPasswordsManager();
+
+			Assert.Throws<ArgumentNullException>("login", () => hasher.PasswordsAreEqual(null, "test", new byte[0]));
+		}
+
+		[Fact]
+		public void PasswordNull()
+		{
+			var hasher = new DoubleSha512HashPasswordsManager();
+
+			Assert.Throws<ArgumentNullException>("password", () => hasher.PasswordsAreEqual("test", null, new byte[0]));
+		}
+
+		[Fact]
+		public void DifferentHashesAreNotEqual()
+		{
+			const string login = "login";
+			const string pass = "pass";
+
+			var hasher = new DoubleSha512HashPasswordsManager();
+
+			var resultBytes = hasher.CreatePassword(login, pass);
+
+			var result = hasher.PasswordsAreEqual($"login_{login}", pass, resultBytes);
+
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void DifferentHashesLengthAreNotEqual()
+		{
+			const string login = "login";
+			const string pass = "pass";
+
+			var hasher = new DoubleSha512HashPasswordsManager();
+
+			var resultBytes = hasher.CreatePassword(login, pass);
+
+			var result = hasher.PasswordsAreEqual(login, pass, resultBytes.TakeWhile((b, i) => i < 10).ToArray());
+
+			Assert.False(result);
 		}
 	}
 }
