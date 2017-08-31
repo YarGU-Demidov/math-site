e335aa89ab03ed1bd69a1a4faf7d4a620f392ad5
Andrey Mokeev <andrey@mokeev1995.ru>
2017-08-31 20:29:17 +0300

    Move common actions to base class  - move methods to base class  - create new constructor with interface

--- tests/MathSite.Tests.Domain/DomainTestsBase.cs
+++ tests/MathSite.Tests.Domain/DomainTestsBase.cs
@@ -1,12 +1,31 @@
-﻿namespace MathSite.Tests.Domain
+﻿using System;
+using System.Threading.Tasks;
+using MathSite.Db;
+
+namespace MathSite.Tests.Domain
 {
 	public abstract class DomainTestsBase
 	{
-		protected readonly ITestDatabaseFactory DatabaseFactory;
+		private readonly ITestDatabaseFactory _databaseFactory;
 
 		public DomainTestsBase()
+			: this(TestSqliteDatabaseFactory.UseDefault())
 		{
-			DatabaseFactory = TestSqlLiteDatabaseFactory.UseDefault();
+		}
+
+		public DomainTestsBase(ITestDatabaseFactory databaseFactory)
+		{
+			_databaseFactory = databaseFactory;
+		}
+
+		protected virtual async Task ExecuteWithContextAsync(Func<IMathSiteDbContext, Task> yourAction)
+		{
+			await _databaseFactory.ExecuteWithContextAsync(yourAction);
+		}
+
+		protected virtual void ExecuteWithContext(Action<IMathSiteDbContext> yourAction)
+		{
+			_databaseFactory.ExecuteWithContext(yourAction);
 		}
 	}
 }
