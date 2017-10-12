eb8422c9d2b431b6fed42dca8076ac2742f6aad9
mokeev1995 <andrey@mokeev1995.ru>
2017-10-13 01:48:51 +0300

    a few minor corrections  * add version to head meta  * add ability to run several instances on different ports/urls

---
+++ src/MathSite/wwwroot/js/blVersion.js
@@ -0,0 +1,15 @@
+﻿Object.defineProperty(window, 'blVersion',
+    {
+        get: function() {
+            var metas = document.querySelectorAll('meta');
+            var version = 'none';
+
+            metas.forEach(it => {
+                if (it.name === 'site-version') {
+                    version = it.content;
+                }
+            });
+                
+            return version;
+        }
+    })
