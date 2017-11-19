6bfa1fcfa2cfb17e0945d3237ff6b9105919bcd0
mokeev1995 <andrey@mokeev1995.ru>
2017-11-19 03:04:32 +0300

    bump versions and fix CI's configs

--- src/MathSite/Migrations/20171118205629_AddDirectory.cs
+++ src/MathSite/Migrations/20171118205629_AddDirectory.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
-using System;
-using System.Collections.Generic;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace MathSite.Migrations
 {
@@ -9,46 +8,46 @@ namespace MathSite.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.AddColumn<Guid>(
-                name: "DirectoryId",
-                table: "File",
-                type: "uuid",
+                "DirectoryId",
+                "File",
+                "uuid",
                 nullable: true);
 
             migrationBuilder.CreateTable(
-                name: "Directory",
-                columns: table => new
+                "Directory",
+                table => new
                 {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    CreationDate = table.Column<DateTime>(type: "timestamp", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
-                    Name = table.Column<string>(type: "text", nullable: true),
-                    RootDirectoryId = table.Column<Guid>(type: "uuid", nullable: true)
+                    Id = table.Column<Guid>("uuid", nullable: false),
+                    CreationDate = table.Column<DateTime>("timestamp", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    Name = table.Column<string>("text", nullable: true),
+                    RootDirectoryId = table.Column<Guid>("uuid", nullable: true)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Directory", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_Directory_Directory_RootDirectoryId",
-                        column: x => x.RootDirectoryId,
-                        principalTable: "Directory",
-                        principalColumn: "Id",
+                        "FK_Directory_Directory_RootDirectoryId",
+                        x => x.RootDirectoryId,
+                        "Directory",
+                        "Id",
                         onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_File_DirectoryId",
-                table: "File",
-                column: "DirectoryId");
+                "IX_File_DirectoryId",
+                "File",
+                "DirectoryId");
 
             migrationBuilder.CreateIndex(
-                name: "IX_Directory_RootDirectoryId",
-                table: "Directory",
-                column: "RootDirectoryId");
+                "IX_Directory_RootDirectoryId",
+                "Directory",
+                "RootDirectoryId");
 
             migrationBuilder.AddForeignKey(
-                name: "FK_File_Directory_DirectoryId",
-                table: "File",
-                column: "DirectoryId",
-                principalTable: "Directory",
+                "FK_File_Directory_DirectoryId",
+                "File",
+                "DirectoryId",
+                "Directory",
                 principalColumn: "Id",
                 onDelete: ReferentialAction.Cascade);
         }
@@ -56,19 +55,19 @@ namespace MathSite.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropForeignKey(
-                name: "FK_File_Directory_DirectoryId",
-                table: "File");
+                "FK_File_Directory_DirectoryId",
+                "File");
 
             migrationBuilder.DropTable(
-                name: "Directory");
+                "Directory");
 
             migrationBuilder.DropIndex(
-                name: "IX_File_DirectoryId",
-                table: "File");
+                "IX_File_DirectoryId",
+                "File");
 
             migrationBuilder.DropColumn(
-                name: "DirectoryId",
-                table: "File");
+                "DirectoryId",
+                "File");
         }
     }
 }
