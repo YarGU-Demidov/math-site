7ebba033ae24b7bde7ab98f12b348b1cf32c18bf
DmitrySelivanov1997 <32522278+DmitrySelivanov1997@users.noreply.github.com>
2018-02-11 23:46:03 +0300

    Update AesEncryptor.cs

--- src/MathSite.Common/Crypto/AesEncryptor.cs
+++ src/MathSite.Common/Crypto/AesEncryptor.cs
@@ -11,12 +11,12 @@ namespace MathSite.Common.Crypto
 
     {
         private readonly byte[] _key;
-        private readonly byte[] _vektor;
+        private readonly byte[] _veсtor;
         public AesEncryptor(IKeyVectorReader keyVectorReader)
         {
             var keyVektorPair = keyVectorReader.GetKeyVector();
             _key = keyVektorPair.Key;
-            _vektor = keyVektorPair.Vector;
+            _veсtor = keyVektorPair.Vector;
         }
         public Task<byte[]> EncryptStringToBytes(string message)
         {
@@ -24,13 +24,13 @@ namespace MathSite.Common.Crypto
                 throw new ArgumentNullException("No message to encrypt");
             if (_key == null || _key.Length <= 0)
                 throw new ArgumentNullException("Key is 0 length");
-            if (_vektor == null || _vektor.Length <= 0)
+            if (_veсtor == null || _veсtor.Length <= 0)
                 throw new ArgumentNullException("Vector is zero length");
             byte[] encrypted;
             using (var aesAlg = Aes.Create())
             {
                 aesAlg.Key = _key;
-                aesAlg.IV = _vektor;
+                aesAlg.IV = _veсtor;
                 var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
                 
                 using (var msEncrypt = new MemoryStream())
@@ -54,13 +54,13 @@ namespace MathSite.Common.Crypto
                 throw new ArgumentNullException("No message to decrypt");
             if (_key == null || _key.Length <= 0)
                 throw new ArgumentNullException("Key is 0 length");
-            if (_vektor == null || _vektor.Length <= 0)
+            if (_veсtor == null || _veсtor.Length <= 0)
                 throw new ArgumentNullException("Vector is zero length");
             string decrypted = null;
             using (Aes aesAlg = Aes.Create())
             {
                 aesAlg.Key = _key;
-                aesAlg.IV = _vektor;
+                aesAlg.IV = _veсtor;
                 var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
                 using (var msDecrypt = new MemoryStream(encryptedMessage))
                 {
