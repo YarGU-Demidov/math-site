a8c7701ec6a2aa55832804454be1ba6bc58da7a1
Dmitry Selivanov <mr.amasing@yandex.ru>
2018-03-21 23:58:57 +0300

    uncommented tests/MathSite.Tests.Common/TwoFactorAutenticationKeyManagerTests.cs

--- tests/MathSite.Tests.Common/TwoFactorAutenticationKeyManagerTests.cs
+++ tests/MathSite.Tests.Common/TwoFactorAutenticationKeyManagerTests.cs
@@ -24,12 +24,12 @@ namespace MathSite.Tests.Common
             var result = await _twoFactorAutenticationKeyManager.CreateEncryptedKey();
             Assert.True(result.IsNotNullOrEmpty());
         }
-        //[Fact]
-        //public async void CreateEncryptedKeyReturnsUniqueBytes()
-        //{
-        //    var result1 = await _twoFactorAutenticationKeyManager.CreateEncryptedKey();
-        //    var result2 = await _twoFactorAutenticationKeyManager.CreateEncryptedKey();
-        //    Assert.NotEqual(result1, result2);
-        //}
+        [Fact]
+        public async void CreateEncryptedKeyReturnsUniqueBytes()
+        {
+            var result1 = await _twoFactorAutenticationKeyManager.CreateEncryptedKey();
+            var result2 = await _twoFactorAutenticationKeyManager.CreateEncryptedKey();
+            Assert.NotEqual(result1, result2);
+        }
     }
 }
