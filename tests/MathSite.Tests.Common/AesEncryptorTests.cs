4ad8e24669d26c922c03cb05930c55adad9d942d
Dmitry Selivanov <mr.amasing@yandex.ru>
2018-03-22 00:04:20 +0300

    uncommented AesEncryptorTests

--- tests/MathSite.Tests.Common/AesEncryptorTests.cs
+++ tests/MathSite.Tests.Common/AesEncryptorTests.cs
@@ -12,49 +12,49 @@ namespace MathSite.Tests.Common
     public class AesEncryptorTests
     {
 
-        //private readonly AesEncryptor _aesEncryptor;
+        private readonly AesEncryptor _aesEncryptor;
 
-        //public AesEncryptorTests()
-        //{
-        //    var path = $"{Environment.CurrentDirectory}/KeyVectorPair";
+        public AesEncryptorTests()
+        {
+            var path = $"{Environment.CurrentDirectory}/KeyVectorPair";
 
-        //    if (!File.Exists(path))
-        //        Program.Main(new[] { path }).Wait();
+            if (!File.Exists(path))
+                Program.Main(new[] { path }).Wait();
 
-        //    _aesEncryptor = new AesEncryptor(new KeyVectorReader($"{Environment.CurrentDirectory}/KeyVectorPair"));
-        //}
-        //[Fact]
-        //public void AesEncryptorHasKeyAndVectorToWorkWith()
-        //{
-        //    Assert.True(_aesEncryptor.Key.IsNotNullOrEmpty());
-        //    Assert.True(_aesEncryptor.Vector.IsNotNullOrEmpty());
-        //}
+            _aesEncryptor = new AesEncryptor(new KeyVectorReader(path));
+        }
+        [Fact]
+        public void AesEncryptorHasKeyAndVectorToWorkWith()
+        {
+            Assert.True(_aesEncryptor.Key.IsNotNullOrEmpty());
+            Assert.True(_aesEncryptor.Vector.IsNotNullOrEmpty());
+        }
 
-        //[Fact]
-        //public async void AesEncryptorEncryptsStringIntoArrayOfBytes()
-        //{
-        //    var rnd = new Random();
-        //    var key = new string(Enumerable.Repeat(rnd, 10).Select(x => (char)x.Next(33, 123)).ToArray());
-        //    var encrypted = await _aesEncryptor.EncryptStringToBytes(key);
-        //    Assert.True(encrypted.IsNotNullOrEmpty());
-        //}
-        //[Fact]
-        //public async void AesEncryptorDecryptsBytesIntoString()
-        //{
-        //    var rnd = new Random();
-        //    var key = new string(Enumerable.Repeat(rnd, 10).Select(x => (char)x.Next(33, 123)).ToArray());
-        //    var encrypted = await _aesEncryptor.EncryptStringToBytes(key);
-        //    var decrypted = await _aesEncryptor.DecryptStringFromBytes(encrypted);
-        //    Assert.NotNull(decrypted);
-        //}
-        //[Fact]
-        //public async void DecryptingEncryptedStringReturnsCorrectValue()
-        //{
-        //    var rnd = new Random();
-        //    var key = new string(Enumerable.Repeat(rnd, 10).Select(x => (char)x.Next(33, 123)).ToArray());
-        //    var encrypted = await _aesEncryptor.EncryptStringToBytes(key);
-        //    var decrypted = await _aesEncryptor.DecryptStringFromBytes(encrypted);
-        //    Assert.Equal(key,decrypted);
-        //}
+        [Fact]
+        public async void AesEncryptorEncryptsStringIntoArrayOfBytes()
+        {
+            var rnd = new Random();
+            var key = new string(Enumerable.Repeat(rnd, 10).Select(x => (char)x.Next(33, 123)).ToArray());
+            var encrypted = await _aesEncryptor.EncryptStringToBytes(key);
+            Assert.True(encrypted.IsNotNullOrEmpty());
+        }
+        [Fact]
+        public async void AesEncryptorDecryptsBytesIntoString()
+        {
+            var rnd = new Random();
+            var key = new string(Enumerable.Repeat(rnd, 10).Select(x => (char)x.Next(33, 123)).ToArray());
+            var encrypted = await _aesEncryptor.EncryptStringToBytes(key);
+            var decrypted = await _aesEncryptor.DecryptStringFromBytes(encrypted);
+            Assert.NotNull(decrypted);
+        }
+        [Fact]
+        public async void DecryptingEncryptedStringReturnsCorrectValue()
+        {
+            var rnd = new Random();
+            var key = new string(Enumerable.Repeat(rnd, 10).Select(x => (char)x.Next(33, 123)).ToArray());
+            var encrypted = await _aesEncryptor.EncryptStringToBytes(key);
+            var decrypted = await _aesEncryptor.DecryptStringFromBytes(encrypted);
+            Assert.Equal(key, decrypted);
+        }
     }
 }
