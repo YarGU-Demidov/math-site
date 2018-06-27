5ed1c0230cbe06d01be9024eb790fe4b9971face
Andrey Mokeev <andrey@mokeev1995.ru>
2018-06-28 01:57:01 +0300

    remove old deps

--- src/MathSite/wwwroot/_references.js
+++ src/MathSite/wwwroot/_references.js
@@ -2,6 +2,3 @@
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
-/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
-/// <reference path="lib/jquery-mousewheel/jquery.mousewheel.js" />
-/// <reference path="lib/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js" />
